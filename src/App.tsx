ca1c3fcb4de933b8b13d1495b6e6dc8a91fd1050
egoing <egoing@gmail.com>
2023-06-15 10:34:13 +0900

    typescript는 js에 type checking 기능을 추가하는 js의 확정된 언어이다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-function Counter() {
+type CounterProps = {
+  title: string;
+  initValue: number;
+};
+function Counter(props: CounterProps) {
+  console.log("props", props);
   return (
     <div>
-      <h1>Counter</h1>
-      <button>+</button> 0
+      <h1>{props.title}</h1>
+      <button>+</button>
+      <button>-</button> {props.initValue}
     </div>
   );
 }
 function App() {
   return (
     <div>
-      <Counter></Counter>
+      <Counter title="손님 카운터" initValue={10}></Counter>
+      <Counter title="불면증 카운터" initValue={20}></Counter>
     </div>
   );
 }
